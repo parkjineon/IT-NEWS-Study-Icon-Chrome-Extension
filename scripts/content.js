a02db3304c829637391a3e21e58fca9ae296c366
parkjineon <truewords0113@naver.com>
2024-01-30 20:48:35 +0900

    Feat: update popup.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,2 @@
 
-const article = document.querySelector('article');
 
-if (article) {
- 
-  const badge = document.createElement('p');
-  badge.textContent = `졸려요 테스트 중`;
-
-  const heading = article.querySelector('h1');
-  const date = article.querySelector('time')?.parentNode;
-  (date ?? heading).insertAdjacentElement('afterend', badge);
-}

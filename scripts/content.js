c2288e19b583cde056ce25754f8ffa232b0344c3
parkjineon <truewords0113@naver.com>
2024-01-31 19:44:00 +0900

    Feat: Try window.onload

--- scripts/content.js
+++ scripts/content.js
@@ -1,2 +1,9 @@
+window.onload = function(){
+    alert('페이지 전체가 로드되었습니다.');
+    const html = document.querySelector('html');
+    const img = html.getElementsByTagName('img')
+    console.log(img)
+    console.log(img.length)
+}
 
-
+//느린거 해결하기,,,, 제발

d44678d877447d3695f61b7e79a54f3bb0030f2f
parkjineon <truewords0113@naver.com>
2024-02-01 09:24:54 +0900

    Feat: Fix HTMLCollection load issue by setTimeout

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,22 @@
 window.onload = function(){
-    alert('페이지 전체가 로드되었습니다.');
-    const html = document.querySelector('html');
-    const img = html.getElementsByTagName('img')
-    console.log(img)
-    console.log(img.length)
+    setTimeout(e=>{
+        alert('페이지 전체가 로드되었습니다.');
+        const html = document.querySelector('html');
+        const img = html.getElementsByTagName('img')
+        console.log(img)
+        console.log(img.length)
+
+
+
+
+
+
+
+
+
+
+
+
+    },10000)
 }
 
-//느린거 해결하기,,,, 제발

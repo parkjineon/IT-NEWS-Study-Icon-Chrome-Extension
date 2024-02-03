6f4e832a7ab47469b062d658c8ac6ecf9f9fd2dd
parkjineon <truewords0113@naver.com>
2024-02-03 13:58:36 +0900

    Feat: onHistoryStateUpdated

--- background.js
+++ background.js
@@ -1,13 +1,7 @@
 
-function refresh() {
-    window.location.reload();
-  };
-chrome.tabs.query({ active: true, currentWindow: true })
-chrome.scripting.executeScript({
-    target:{
-        tabId: tabs[0].id,
-        files: refresh
-    }
+
+
+chrome.webNavigation.onHistoryStateUpdated.addListener((data)=>{
+    console.log("History Stated Updated ")
 })
 
-// chrome extension 공부

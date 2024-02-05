fc1d4edd5bf458cb1982d3f923eef29d77858c38
parkjineon <truewords0113@naver.com>
2024-02-05 21:58:36 +0900

    Feat: Add ExecuteScript

--- background.js
+++ background.js
@@ -3,5 +3,20 @@
 
 chrome.webNavigation.onHistoryStateUpdated.addListener((data)=>{
     console.log("History Stated Updated ")
+    if(data.url === "https://www.notion.so/32d985e76cad4e1f96b101285e6500b2?v=460dc7543d1a4b4194e22d5460662666"){
+        console.log(data);
+        
+        chrome.scripting.executeScript({
+            target: {tabId: data.tabId},
+            files : ["./scripts/content.js"]
+        })
+
+
+        chrome.scripting.insertCSS({
+            target: {tabId: data.tabId},
+            files:["./scripts/content.css"]
+        })
+
+    }
 })
 
